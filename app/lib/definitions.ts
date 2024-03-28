1e00c0a9877ecceba6cbe36b59b7b50a377fdadc
HMZElidrissi <hamza.ezzharelidrissi1@gmail.com>
2024-03-28 16:26:29 +0000

    setup project 🚀

---
+++ app/lib/definitions.ts
@@ -0,0 +1,11 @@
+// This file contains type definitions for your data.
+// It describes the shape of the data, and what data type each property should accept.
+
+export type User = {
+  id: number;
+  name: string;
+  email: string;
+  role: string;
+};
+
+
