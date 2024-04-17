7213369f9fd399e83e182c0224d7b6fe9f10a196
HMZElidrissi <hamza.ezzharelidrissi1@gmail.com>
2024-04-17 16:03:43 +0100

    feat: urgent blood request notification

--- todo.ts
+++ todo.ts
@@ -2,7 +2,6 @@
 // TODO: Generate user avatar
 // TODO: Update the use case diagram (s'authentifier => include) and class diagram
 // TODO: Make sequence diagram for authentication
-// TODO: forgot password
 // TODO: sign out as a dialog
 // TODO: Add a loading spinner
 // TODO: the donate button should be changed if a user is authenticated
